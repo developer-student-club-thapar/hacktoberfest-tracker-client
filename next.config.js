7725acb40207b05e7269ec8d7a6caff47e009ff5
arshi45 <arshdeep0220@gmail.com>
2022-11-16 00:05:28 +0530

    Components

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects(){
+    return [
+      {
+        source: '/',
+        destination: '/access',
+        permanent: true,
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
