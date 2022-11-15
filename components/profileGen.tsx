2ada81219deb0aab6961a13201cd2485fb33d556
Nikhil Bakshi <nikhilbakshi@Nikhils-MacBook-Pro-2.local>
2022-11-16 01:39:48 +0530

    Left component with profile structure

---
+++ components/profileGen.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import hacktober from '../public/hacktober.png';
+import { AiFillGithub, AiFillTwitterCircle } from 'react-icons/ai';
+
+const ProfileCard= ()=>{
+    return(
+        <div className=" flex flex-col justify-center items-center py-[32px] px-[20px]
+         bg-componentLight w-fit rounded-xl shadowProfile">
+            
+            <div className="mb-[16px] ">
+                <Image className="rounded-full" src={hacktober} alt={'ok'} width={80}/>
+            </div>
+
+            <div className="flex flex-col items-center">
+                <h1 className="font-sans font-semibold text-2xl">Servatom</h1>
+                <h3 className="font-sans text-xl">Building Something Great!</h3>
+                <div className="flex flex-row gap-[10px] text-[20px]">
+                    <a href="https://google.com"><AiFillGithub /></a>
+                    <a href="https://google.com"><AiFillTwitterCircle /></a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProfileCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import Top from "../components/top"
+import Left from "../components/left"
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col">
       <Top/>
+      <div className="flex flex-row w-screen">
+        <div className="w-full">
+
+        </div>
+        <Left/>
+      </div>
     </div>
   )
 }
