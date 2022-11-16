365068dcb52434e472eef63de65c96531cc30a01
arshi45 <arshdeep0220@gmail.com>
2022-11-16 16:05:56 +0530

    Changes

--- components/sidebar/sidebarWrapper.tsx
+++ components/sidebar/sidebarWrapper.tsx
@@ -6,7 +6,7 @@ const Sidebar = ({orgName,data}) => {
 
     return (
         <div className='drop-shadow-xl h-4/5 text-xl w-1/5 bg-componentLight flex flex-col text-slate-600'>
-            <button className="font-semibold text-2xl my-6 bg-slate-300">DSC TIET<FontAwesomeIcon icon={faCaretUp}/></button>
+            <button className="font-semibold text-2xl my-6 bg-slate-300">{orgName}<FontAwesomeIcon icon={faCaretUp}/></button>
             {data.map((repo:any) => {
                     return (
                         <List name={repo.name} key={Math.random()}/> 
---
+++ pages/index/[org].tsx
@@ -0,0 +1,56 @@
+import Top from "../../components/top"
+import Left from "../../components/left"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import Sidebar from "../../components/sidebar/sidebarWrapper"
+
+
+export default function Home() {
+
+  const router = useRouter();
+    const {org} = router.query;
+    const url = "http://localhost:3060/"+org
+    const [data,setData] = useState({
+        org:{
+          orgName:'',
+          orgDesc:''
+        },
+        data:{
+          commits:0,
+          issues:0,
+          contributors:0,
+          repoCount:0,
+          repos:[]
+      }
+    });
+
+    useEffect(() => {
+        fetch(url,{
+            'method': 'GET',
+            'headers': {
+                Accept: 'application/json',
+            'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            setData(data);
+        }
+        );
+        // console.log(url);
+        
+    },[]);
+
+  return (
+    <div className="flex flex-col">
+      <Top/>
+      <div className="flex flex-row w-screen">
+        <Sidebar orgName={data.org.orgName} data={data.data.repos}/>
+        <div className="w-full">
+
+        </div>
+        <Left/>
+      </div>
+    </div>
+  )
+}
