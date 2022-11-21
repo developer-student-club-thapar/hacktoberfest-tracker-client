8a542ea9acbcd58362c80c206478bc128bd48f1e
arshi45 <arshdeep0220@gmail.com>
2022-11-21 12:37:28 +0530

    Navbar logic

--- components/sidebar/repoNavigation.tsx
+++ components/sidebar/repoNavigation.tsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 
-const List = ({name}) => {
+const List = ({name,index,check,setCheck}) => {
 
     const [list,setList] = useState(false);
-
-    const handleClick = () => {
-        setList(!list)
+    
+    const handleClick = (e) => {
+        if(index == check.key)
+            setCheck({key:-1})
+        else
+            setCheck({key:index})
     }
 
-    const s='bg-navButtonLight text-[#fff] text-left text-xl w-full py-2 px-2 w-full flex flex-col items-start';
-    const u='bg-componentLight text-[#000] text-left text-xl w-full py-2 px-2 w-full flex flex-col items-start';
-
-    const sn='text-left text-base py-1 px-2 bg-navButtonNestedLight w-full text-left';
-    const un= 'text-left text-base py-1 px-2 w-full text-left'
+    const s='bg-navButtonLight text-[#fff] text-left text-xl w-full py-2 px-2';
+    const u='bg-componentLight text-[#000] text-left text-xl w-full py-2 px-2 ';
 
     return (
-        <div className={list?s:u}>
-            <button className="mb-1" onClick={handleClick}>{name.slice(0,1).toUpperCase()+name.slice(1,name.length)}</button>
-            {list?
-                <div className="flex flex-col items-start w-full mt-1">
-                    <button className="text-left text-base py-1 px-2 bg-slate-300 bg-navButtonNestedLight w-full text-left">Dashboard</button>
+        <div className="w-full flex flex-col items-start">
+            <button className={check.key==index?s:u} onClick={handleClick} value={index} >{name.slice(0,1).toUpperCase()+name.slice(1,name.length)}</button>
+            {check.key==index?
+                <div className="flex flex-col items-start">
+                    <button className="font-light text-base py-1 px-2 bg-slate-300">Dashboard</button>
                     <button className="font-light text-base py-1 px-2 bg-slate-300">Issues</button>
                     <button className="font-light text-base py-1 px-2 bg-slate-300">Contributors</button>
                 </div>
--- components/sidebar/sidebarWrapper.tsx
+++ components/sidebar/sidebarWrapper.tsx
@@ -4,8 +4,15 @@ import List from "./repoNavigation";
 const Sidebar = ({orgName,data}) => {
 
     const [selected, setSelected]= useState(true);
+    const [highlight,setHighlight] = useState({
+        key:-1
+    })
     const handleClick = () => {
-        setSelected(!selected);
+        if(highlight.key == data.length+1)
+            setHighlight({key:-1})
+        else
+            setHighlight({key:data.length+1})
+
     }
 
     const s='bg-navButtonLight text-[#fff] text-left text-xl w-full py-2 px-2';
@@ -13,10 +20,10 @@ const Sidebar = ({orgName,data}) => {
 
     return (
         <div className='h-full w-[22vw] bg-componentLight flex flex-col py-7 shadowProfile rounded-xl'>
-            <button className={selected?s:u} onClick={handleClick}>{orgName}</button>
-            {data.map((repo:any) => {
+            <button className={highlight.key == data.length+1?s:u} onClick={handleClick}>{orgName}</button>
+            {data.map((repo:any,index:number) => {
                     return (
-                        <List name={repo.name} key={Math.random()}/> 
+                        <List name={repo.name} key={Math.random()} index={index} check={highlight} setCheck={setHighlight}/> 
                     );
                 })
             }
