554f8dc36ae951988894689742e6182e7c5c151f
Nikhil Bakshi <nikhilbakshi@Nikhils-MacBook-Pro-2.local>
2022-11-21 12:46:13 +0530

    Minor style updates v2

--- components/sidebar/repoNavigation.tsx
+++ components/sidebar/repoNavigation.tsx
@@ -11,6 +11,7 @@ const List = ({name,index,check,setCheck}) => {
             setCheck({key:index})
     }
 
+<<<<<<< Updated upstream
     const s='bg-navButtonLight text-[#fff] text-left text-xl w-full py-2 px-2';
     const u='bg-componentLight text-[#000] text-left text-xl w-full py-2 px-2 ';
 
@@ -20,6 +21,20 @@ const List = ({name,index,check,setCheck}) => {
             {check.key==index?
                 <div className="flex flex-col items-start">
                     <button className="font-light text-base py-1 px-2 bg-slate-300">Dashboard</button>
+=======
+    const s='bg-navButtonLight text-[#fff] text-left text-xl w-full py-2 px-2 w-full flex flex-col items-start';
+    const u='bg-componentLight text-[#000] text-left text-xl w-full py-2 px-2 w-full flex flex-col items-start';
+
+    const sn='text-left text-base py-1 px-2 bg-navButtonNestedLight w-full text-left';
+    const un= 'text-left text-base py-1 px-2 w-full text-left'
+
+    return (
+        <div className={list?s:u}>
+            <button className="mb-1" onClick={handleClick}>{name.slice(0,1).toUpperCase()+name.slice(1,name.length)}</button>
+            {list?
+                <div className="flex flex-col items-start w-full mt-1">
+                    <button className="text-left text-base py-1 px-2 bg-slate-300 bg-navButtonNestedLight w-full text-left">Dashboard</button>
+>>>>>>> Stashed changes
                     <button className="font-light text-base py-1 px-2 bg-slate-300">Issues</button>
                     <button className="font-light text-base py-1 px-2 bg-slate-300">Contributors</button>
                 </div>
--- components/sidebar/sidebarWrapper.tsx
+++ components/sidebar/sidebarWrapper.tsx
@@ -4,6 +4,7 @@ import List from "./repoNavigation";
 const Sidebar = ({orgName,data}) => {
 
     const [selected, setSelected]= useState(true);
+<<<<<<< Updated upstream
     const [highlight,setHighlight] = useState({
         key:-1
     })
@@ -13,6 +14,10 @@ const Sidebar = ({orgName,data}) => {
         else
             setHighlight({key:data.length+1})
 
+=======
+    const handleClick = () => {
+        setSelected(!selected);
+>>>>>>> Stashed changes
     }
 
     const s='bg-navButtonLight text-[#fff] text-left text-xl w-full py-2 px-2';
@@ -20,8 +25,17 @@ const Sidebar = ({orgName,data}) => {
 
     return (
         <div className='h-full w-[22vw] bg-componentLight flex flex-col py-7 shadowProfile rounded-xl'>
+<<<<<<< Updated upstream
             <button className={highlight.key == data.length+1?s:u} onClick={handleClick}>{orgName}</button>
             {data.map((repo:any,index:number) => {
+=======
+            
+            <h3 className="font-medium text-[#a3a3a3] ml-1 mb-1">Organisation</h3>
+            <button className={selected?s:u} onClick={handleClick}>{orgName}</button>
+
+            <h3 className="mt-4 font-medium text-[#a3a3a3] ml-1 mb-1">Repositories</h3>
+            {data.map((repo:any) => {
+>>>>>>> Stashed changes
                     return (
                         <List name={repo.name} key={Math.random()} index={index} check={highlight} setCheck={setHighlight}/> 
                     );
